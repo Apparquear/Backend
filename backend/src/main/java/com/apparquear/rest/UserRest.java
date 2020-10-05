382b3ec73cbe7576e03f25a87891fe62a7f4efa3
Brayan Hurtado <bohurtadop@unal.edu.co>
2020-10-05 13:27:32 -0500

    Adding find all users method

--- backend/src/main/java/com/apparquear/rest/UserRest.java
+++ backend/src/main/java/com/apparquear/rest/UserRest.java
@@ -1,6 +1,9 @@
 package com.apparquear.rest;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +23,11 @@ public class UserRest {
 	@PostMapping("/save")
 	public void save(@RequestBody User user) {
 		userDAO.save(user);
+	}
+	
+	@GetMapping("/findAll")
+	public List<User> findAll(){
+		return userDAO.findAll();
 	}
 
 }
