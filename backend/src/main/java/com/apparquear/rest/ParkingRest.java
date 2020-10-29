b8c5e9d0cc855e22b3ea265ccee1e8322f5f7dcd
mmolinaf <mmolinaf@unal.edu.co>
2020-10-28 21:45:47 -0500

    :hammer: Busqueda espacios disponibles bahias

--- backend/src/main/java/com/apparquear/rest/ParkingRest.java
+++ backend/src/main/java/com/apparquear/rest/ParkingRest.java
@@ -1,12 +1,16 @@
 package com.apparquear.rest;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apparquear.dao.ParkingDAO;
@@ -29,5 +33,16 @@ public class ParkingRest {
 	@GetMapping("/findAll")
 	public List<Parking> findAll(){
 		return parkingDAO.findAll();
+	}
+
+	@GetMapping("/cupos/{id}")
+	@ResponseBody
+	public List<Integer> findCupos(@PathVariable Integer id){
+		List<Integer> ret = new ArrayList<>();
+		Parking cosa = parkingDAO.findCupo(id);
+		ret.add(cosa.getCar_spaces_available());
+		ret.add(cosa.getMotorcycle_spaces_available());
+		ret.add(cosa.getBike_spaces_available());
+		return ret;
 	}
 }
