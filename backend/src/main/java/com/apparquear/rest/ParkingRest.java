09c5fbb91f1b70c03ee9892ecd25b31e507ab3ce
mmolinaf <mmolinaf@unal.edu.co>
2020-11-02 22:10:06 -0500

    Cambios consultar bahias

--- backend/src/main/java/com/apparquear/rest/ParkingRest.java
+++ backend/src/main/java/com/apparquear/rest/ParkingRest.java
@@ -3,6 +3,7 @@ package com.apparquear.rest;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,10 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apparquear.dao.ParkingDAO;
+import com.apparquear.exception.ApiRequestException;
 import com.apparquear.model.Parking;
 
 @RestController
@@ -22,8 +23,8 @@ import com.apparquear.model.Parking;
 public class ParkingRest {
 	@Autowired
 	private ParkingDAO parkingDAO;
-	
-	//Methods
+
+	// Methods
 	@CrossOrigin
 	@PostMapping("/save/{user_id}/{token}")
 	public void save(@PathVariable Long user_id, @PathVariable String token, @RequestBody Parking parking) {
@@ -37,23 +38,31 @@ public class ParkingRest {
 		parking.setMotorcycle_spaces_available(parking.getMotorcycle_spaces());
 		parking.setScore(0.0);
 		parking.setTotal_spaces(parking.getBike_spaces() + parking.getCar_spaces() + parking.getMotorcycle_spaces());
-		parking.setTotal_spaces_available(parking.getBike_spaces() + parking.getCar_spaces() + parking.getMotorcycle_spaces());
+		parking.setTotal_spaces_available(
+				parking.getBike_spaces() + parking.getCar_spaces() + parking.getMotorcycle_spaces());
 		parkingDAO.save(parking);
 	}
-	
+
 	@GetMapping("/findAll")
-	public List<Parking> findAll(){
+	public List<Parking> findAll() {
 		return parkingDAO.findAll();
 	}
 
-	@GetMapping("/cupos/{id}")
-	@ResponseBody
-	public List<Integer> findCupos(@PathVariable Integer id){
+	@CrossOrigin
+	@PostMapping("/cupos")
+	public List<Integer> findCupos(@RequestBody Parking park) {
 		List<Integer> ret = new ArrayList<>();
-		Parking cosa = parkingDAO.findCupo(id);
-		ret.add(cosa.getCar_spaces_available());
-		ret.add(cosa.getMotorcycle_spaces_available());
-		ret.add(cosa.getBike_spaces_available());
+		Parking parking = new Parking();
+		Optional<Parking> optionalParking = parkingDAO.findById(park.getParking_ID());
+		
+		if (optionalParking != null) {
+			parking = optionalParking.get();
+			ret.add(parking.getCar_spaces_available());
+			ret.add(parking.getMotorcycle_spaces_available());
+			ret.add(parking.getBike_spaces_available());
+		} else {
+			throw new ApiRequestException("Este parqueadero no se encuentra registrado");
+		}
 		return ret;
 	}
 }
