b2e4e048fd381ef412d37d24edd49545cbff294c
Brayan Hurtado <bohurtadop@unal.edu.co>
2020-11-04 12:17:20 -0500

    Eliminando comments

--- backend/src/main/java/com/apparquear/dao/ParkingDAO.java
+++ backend/src/main/java/com/apparquear/dao/ParkingDAO.java
@@ -1,12 +1,7 @@
 package com.apparquear.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.jpa.repository.Query;
-
-
 import com.apparquear.model.Parking;
 
 public interface ParkingDAO extends JpaRepository<Parking, Long>{
-    // @Query("SELECT p  FROM Parking p WHERE p.parking_ID = ?1")
-    // Parking findCupo(long idParking );
 }
